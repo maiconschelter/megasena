79a79a3e757e4e4384b44947b013f8013ac100ba
Maicon Schelter <maiconschelter@Macbook-Pro-2.local>
2018-08-31 23:04:43 -0300

    Create a sql query with the statistics

---
+++ megasena.sql
@@ -0,0 +1,22 @@
+﻿/*top six numbers sorted*/
+SELECT * FROM (
+  SELECT COUNT(sorted), sorted
+    FROM (
+      SELECT unnest(resnumber) AS sorted
+        FROM public.results) AS parser
+   GROUP BY sorted
+   ORDER BY 1 DESC
+   LIMIT 6
+) AS origem
+ORDER BY 2 ASC
+
+/*sorts having top numbers*/
+SELECT *
+  FROM public.results
+ WHERE (resnumber @> '{10}' AND resnumber @> '{5}')
+   AND (resnumber @> '{53}' OR resnumber @> '{23}' OR resnumber @> '{54}' OR resnumber @> '{4}')
+
+/*the magic numbers*/
+SELECT *
+  FROM public.results
+ WHERE resnumber @> '{4,5,10,46,53,54}'
